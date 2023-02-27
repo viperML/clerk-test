904eaeb39534da9a336211827dd6a9e8c13f438a
Fernando Ayats <ayatsfer@gmail.com>
2023-02-27 22:28:22 +0000

    plotting

--- notebooks/main.clj
+++ notebooks/main.clj
@@ -1,5 +1,6 @@
 ;; # Bienvenido
-(ns main)
+(ns main
+  (:require [nextjournal.clerk :as clerk]))
 
 (require '[meta-csv.core :as csv])
 (require '[nextjournal.clerk :as clerk])
@@ -7,4 +8,19 @@
 
 (def parsed (csv/read-csv "./execution_1_parsed.csv"))
 
-(clerk/table parsed)
+(def data (->> parsed
+               (map #(sorted-map :id (get % :FunctionId)
+                                 :duration (get % :FunctionDuration)
+                                 :delay (get % :InvocationDelay)))))
+
+(clerk/table data)
+
+(clerk/vl
+ {:data {:values data}
+  :width 500
+  :height 500
+  :mark {:type "point"}
+  :encoding {:x {:field :id
+                  :type "quantitative"}
+              :y {:field :duration
+                  :type "quantitative"}}})
--- shell.nix
+++ shell.nix
@@ -1,6 +1,6 @@
 let
   pkgs = import <nixpkgs> {};
-  jdk = pkgs.jdk17;
+  jdk = pkgs.jdk17_headless;
 in
   pkgs.mkShell.override {stdenv = pkgs.stdenvNoCC;} pkgs.mkShell {
     packages = [
